c169e0241bbd64ad38a2300770908d023c248966
eliasinacio <0.elias.inacio@gmail.com>
2022-10-18 23:17:18 -0300

    added external fonts

--- Components/Amount.js
+++ Components/Amount.js
@@ -1,12 +1,21 @@
 import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { useFonts } from "expo-font"
 
 export default function Amount () {
+    const [loaded] = useFonts({
+        Montserrat: require('../assets/fonts/Silkscreen/Silkscreen-Regular.ttf'),
+    });
+
+    if (!loaded) {
+        return null;
+    }
+
     return (
         <View style={styles.amountView}>
             <Text style={styles.amount}>
                 <Text style={styles.amountSign}>R$</Text>
-                <Text style={styles.amountIntegerValue}>2000,</Text>
-                <Text style={styles.amountDecimalValue}>00</Text>
+                <Text style={styles.amountIntegerValue}>129054,</Text>
+                <Text style={styles.amountDecimalValue}>99</Text>
             </Text>
         </View>
     )
@@ -16,21 +25,32 @@ const width = Dimensions.get('window').width
 
 const styles = StyleSheet.create({
     amountView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: 'transparent',
         alignSelf: 'center',
         width: width,
         minHeight: 110,
         padding: 20,
+        fontFamily: 'Silkscreen-Regular',
     }, 
     amount: {
-        flex: 5,
+        flex: 1,
         textAlign: 'center',
         color: '#111119',
+        fontFamily: 'Silkscreen-Regular',
         fontSize: 48,
+        
+        minWidth: '80%',
+
+        borderBottomColor: '#111',
+        borderBottomWidth: 3,
     },
     amountSign: {
         fontSize: 24,
         alignSelf: 'flex-start',
+        fontFamily: 'Silkscreen-Regular',
         padding: 5,
     },
     amountIntegerValue: {
